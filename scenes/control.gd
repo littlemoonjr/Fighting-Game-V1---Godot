360ee3409077009c897e1f9dca69b77a10a5f532
Moon <gdowna@gmail.com>
2026-04-18 20:21:09 -0400

    new ui, still working on it

--- scenes/control.gd
+++ scenes/control.gd
@@ -8,4 +8,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$Label.text = "FPS: " + str(Engine.get_frames_per_second())
+	$FPSCounter.text = "FPS: " + str(Engine.get_frames_per_second())
